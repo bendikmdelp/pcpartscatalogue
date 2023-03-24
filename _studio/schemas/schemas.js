7f7f532332ef4337989d4033dea88206644b09c6
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2023-03-24 14:40:21 +0100

    Created schemas

--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,1 +1,9 @@
-export default []
+import brand from "./documents/brand.js";
+import category from "./documents/category.js";
+import product from "./documents/product.js";
+
+export default [
+	product,
+	category,
+	brand
+]
