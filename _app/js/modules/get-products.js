a88233fe9a898fe3feae568728e54555d9d69d94
WaffleChampion <bendikmdelp@gmail.com>
2023-04-05 11:55:11 +0200

    added and styled product cards

--- _app/js/modules/get-products.js
+++ _app/js/modules/get-products.js
@@ -1,7 +1,11 @@
 import {sanity} from "../sanity.js";
 
 export async function getAllProducts() {
-	const query = `*[_type == 'product']`;
+	const query = `*[_type == 'product'] {
+		name,
+    price,
+  "image": image.asset-> url
+	}`;
 
 	const params = {
 
@@ -9,7 +13,38 @@ export async function getAllProducts() {
 
 	const products = await sanity.fetch(query);
 
-	console.log(products)
+
+	renderHTML();
+
+	function renderHTML() {
+		const productsContainer = document.querySelector('.products__list');
+
+		for(const product of products) {
+			const productItem = createProductItemDOM(product);
+
+			productsContainer.append(productItem);
+		}
+	}
+
+	function createProductItemDOM(product) {
+		const productItem = document.createElement('div');
+		const productImage = document.createElement('img');
+		const productTitle = document.createElement('p');
+		const productPrice = document.createElement('p');
+
+		productItem.className = 'products__list-item';
+		productImage.className = 'products__list-item-image';
+		productTitle.className = 'products__list-item-title';
+		productPrice.className = 'products__list-item-price';
+
+		productImage.src=product.image;
+		productTitle.innerText = product.name;
+		productPrice.innerText = product.price;
+
+		productItem.append(productImage, productTitle, productPrice);
+
+		return productItem;
+	}
 
 	//return products
 }
