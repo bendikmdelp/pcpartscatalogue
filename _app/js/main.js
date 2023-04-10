08f1f2c38cc4037855769207fe94e82ab0165a1a
WaffleChampion <bendikmdelp@gmail.com>
2023-04-10 10:17:37 +0200

    added fetch caregories and sort by category

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,5 +1,7 @@
 import { getALLBrands } from "./modules/get-brands.js";
+import { getALLCategories } from "./modules/get-categories.js";
 import { getAllProducts } from "./modules/get-products.js";
 
 const products = await getAllProducts();
 getALLBrands(products);
+getALLCategories(products);
