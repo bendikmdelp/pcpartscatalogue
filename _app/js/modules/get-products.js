e58844d6b6d5d8554570749cb0464621c349239e
WaffleChampion <bendikmdelp@gmail.com>
2023-04-10 09:53:21 +0200

    moved render functions to render file

--- _app/js/modules/get-products.js
+++ _app/js/modules/get-products.js
@@ -1,4 +1,5 @@
 import {sanity} from "../sanity.js";
+import { createProductListDOM } from "./render.js";
 
 export async function getAllProducts() {
 	const query = `*[_type == 'product'] {
@@ -17,34 +18,10 @@ export async function getAllProducts() {
 	renderHTML();
 
 	function renderHTML() {
-		const productsContainer = document.querySelector('.products__list');
-
-		for(const product of products) {
-			const productItem = createProductItemDOM(product);
-
-			productsContainer.append(productItem);
-		}
+		createProductListDOM(products)
 	}
 
-	function createProductItemDOM(product) {
-		const productItem = document.createElement('div');
-		const productImage = document.createElement('img');
-		const productTitle = document.createElement('p');
-		const productPrice = document.createElement('p');
-
-		productItem.className = 'products__list-item';
-		productImage.className = 'products__list-item-image';
-		productTitle.className = 'products__list-item-title';
-		productPrice.className = 'products__list-item-price';
-
-		productImage.src=product.image;
-		productTitle.innerText = product.name;
-		productPrice.innerText = product.price;
-
-		productItem.append(productImage, productTitle, productPrice);
-
-		return productItem;
-	}
+	
 
 	return products
 }
---
+++ _app/js/modules/render.js
@@ -0,0 +1,31 @@
+function createProductItemDOM(product) {
+	const productItem = document.createElement('div');
+	const productImage = document.createElement('img');
+	const productTitle = document.createElement('p');
+	const productPrice = document.createElement('p');
+
+	productItem.className = 'products__list-item';
+	productImage.className = 'products__list-item-image';
+	productTitle.className = 'products__list-item-title';
+	productPrice.className = 'products__list-item-price';
+
+	productImage.src=product.image;
+	productTitle.innerText = product.name;
+	productPrice.innerText = product.price;
+
+	productItem.append(productImage, productTitle, productPrice);
+
+	return productItem;
+}
+
+function createProductListDOM(products) {
+	const productsContainer = document.querySelector('.products__list');
+
+		for(const product of products) {
+			const productItem = createProductItemDOM(product);
+
+			productsContainer.append(productItem);
+		}
+}
+
+export {createProductListDOM}
