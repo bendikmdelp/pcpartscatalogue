51da6ffc5147b613b84f0560330a1e7825f503a2
WaffleChampion <bendikmdelp@gmail.com>
2023-04-12 09:46:20 +0200

    added show 6 random products

--- _app/js/modules/get-products.js
+++ _app/js/modules/get-products.js
@@ -19,7 +19,42 @@ export async function getAllProducts() {
 	renderHTML();
 
 	function renderHTML() {
+		const newArray = createArray();
 		createProductListDOM(products)
+		showRandomProducts(newArray);
+	}
+
+	function createArray() {
+		const newArray = [];
+		console.log('new')
+
+		for(let index = 0; newArray.length < 6; index++) {
+			const min = Math.ceil(0);
+			const max = Math.floor(products.length);
+		
+			let randomNumber = Math.floor(Math.random() * (max-min) + min);
+
+			if(newArray.includes(randomNumber)){
+				createArray();
+			}else {
+				newArray.push(randomNumber)
+			}
+
+		}
+
+		return newArray;
+	}
+
+	function showRandomProducts(array) {
+		console.log(array)
+		const productNodeList = document.querySelectorAll('.products__list-item')
+		for(const [index, product] of productNodeList.entries()){
+			product.classList.add('products__list-item--hidden');
+		}
+
+		for(const arrayItem of array) {
+			productNodeList[arrayItem].classList.remove('products__list-item--hidden')
+		}
 	}
 
 	return products
