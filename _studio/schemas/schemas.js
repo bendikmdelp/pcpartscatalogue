25ad155d37a7079cd842e4e59c44f34ea60815b5
WaffleChampion <bendikmdelp@gmail.com>
2023-04-12 11:01:04 +0200

    updated sanity structure

--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,9 +1,11 @@
 import brand from "./documents/brand.js";
 import category from "./documents/category.js";
 import product from "./documents/product.js";
+import settings from "./documents/settings.js";
 
 export default [
 	product,
 	category,
-	brand
+	brand,
+	settings
 ]
