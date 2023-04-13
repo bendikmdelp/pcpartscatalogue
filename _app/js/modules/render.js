e163115e104b01a0b91ed0ebd820ca715dd72ef6
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2023-04-13 23:47:29 +0200

    Added styling and some functions

--- _app/js/modules/render.js
+++ _app/js/modules/render.js
@@ -7,30 +7,37 @@ function createProductItemDOM(product, index) {
 	const productImage = document.createElement('img');
 	const productTitle = document.createElement('p');
 	const productPrice = document.createElement('p');
+	const stringPrice = product.price/100;
+	const spacedString = addSpaces(stringPrice);
 
+	
 	productItem.className = 'products__list-item';
 	productImage.className = 'products__list-item-image';
 	productTitle.className = 'products__list-item-title';
 	productPrice.className = 'products__list-item-price';
-
+	
 	productItem.setAttribute('data-index', index)
-
-	productImage.src=product.image;
+	
+	productImage.src = product.image;
 	productTitle.innerText = product.name;
-	productPrice.innerText = `${product.price/100}Kr`;
-
+	productPrice.innerText = `${spacedString} kr`;
+	
 	productItem.append(productImage, productTitle, productPrice);
-
+	
 	productItem.addEventListener('click', handleProductCardClick);
-
+	
 	return productItem;
+}
+
+function addSpaces(number) {
+	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
 
 function createProductListDOM(products) {
 	productList = products;
 	const productsContainer = document.querySelector('.products__list');
 	productsContainer.innerHTML = ''
-
+	
 		for(const [index, product] of products.entries()) {
 			const productItem = createProductItemDOM(product,index);
 
